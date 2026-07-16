8b368d3f970b15f9349c234cdd31b4bfc97bbb11
dudujerry <dudujerry452@qq.com>
2026-07-17 01:16:43 +0800

    add openmp parallel

--- src/rendering/renderer.cpp
+++ src/rendering/renderer.cpp
@@ -1,5 +1,6 @@
 #include "renderer.h"
 #include <iostream>
+#include <atomic>
 
 namespace mupsi
 {
@@ -37,9 +38,12 @@ namespace mupsi
 
   void GPRenderer::render(const GPScene &scene, const Camera &camera)
   {
+    int total = fb_.height() * fb_.width();
+    std::atomic<int> done{0};
+
+    #pragma omp parallel for collapse(2)
     for (int j = 0; j < fb_.height(); j++)
     {
-      std::cout << "start" << j << " " << std::endl;
       for (int i = 0; i < fb_.width(); i++)
       {
 
@@ -53,12 +57,19 @@ namespace mupsi
         Intersection its = scene.castRay(ray);
         if (its.hit)
         {
-          Vector3f light(-1, -1, -0.5); 
-          Vector3f lightemit(0.0, 1.0, 0.0); 
-          fb_(i, j) = Color({lightemit * std::max(0.0f, (light.normalized()).dot(its.normal))}); 
+          Vector3f light(-1, -1, -0.5);
+          Vector3f lightemit(0.0, 1.0, 0.0);
+          fb_(i, j) = Color({lightemit * std::max(0.0f, (light.normalized()).dot(its.normal))});
+        }
+
+        int n = ++done;
+        if (n % (total / 100) == 0) {
+          #pragma omp critical
+          std::cerr << "\r" << (100 * n / total) << "%" << std::flush;
         }
       }
     }
+    std::cerr << "\r100%" << std::endl;
   }
 
 
